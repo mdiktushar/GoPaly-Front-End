4af4473a2bdba464e52b5649584ae3544f690351
mdiktushar <md.tushar1804@gmail.com>
2023-05-19 00:04:59 +0600

    tuchups on welcome page

--- src/pages/Home/components/Welcome.jsx
+++ src/pages/Home/components/Welcome.jsx
@@ -2,17 +2,24 @@ import React from "react";
 
 const Welcome = () => {
   return (
-    <p className="text-center mt-20">
-      Welcome to the magical world of the Disney Toy Shop! Here, dreams come
-      true and imagination knows no bounds. Our enchanting online store is
-      filled with a delightful array of Disney toys, perfect for kids and
-      collectors alike. From classic characters like Mickey Mouse and Cinderella
-      to new favorites like Elsa and Buzz Lightyear, we offer a wide range of
-      beloved Disney merchandise. Whether you're searching for plush toys,
-      action figures, puzzles, or costumes, we have something to bring joy to
-      every Disney fan. Join us on this enchanting journey and create memories
-      that will last a lifetime. Start exploring the Disney Toy Shop today!
-    </p>
+    <>
+      <h2 className="text-5xl font-semibold text-gray-600 text-center mt-10">
+        Welcome
+      </h2>
+      <hr />
+      <p className="text-center mt-10">
+        Welcome to the magical world of the Disney Toy Shop! Here, dreams come
+        true and imagination knows no bounds. Our enchanting online store is
+        filled with a delightful array of Disney toys, perfect for kids and
+        collectors alike. From classic characters like Mickey Mouse and
+        Cinderella to new favorites like Elsa and Buzz Lightyear, we offer a
+        wide range of beloved Disney merchandise. Whether you're searching for
+        plush toys, action figures, puzzles, or costumes, we have something to
+        bring joy to every Disney fan. Join us on this enchanting journey and
+        create memories that will last a lifetime. Start exploring the Disney
+        Toy Shop today!
+      </p>
+    </>
   );
 };
 
