c7a850235748154e0b50f15c64bf291a4b1fa2ae
mdiktushar <md.tushar1804@gmail.com>
2023-05-18 10:09:59 +0600

    footer

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/Home/Home';
+import MainLayout from '../layout/MainLayout/MainLayout';
 
 const routes = createBrowserRouter([
     {
         path: `/`,
-        element: <Home />
+        element: <MainLayout />,
+        children: [
+            {
+                path: `/`,
+                element: <Home />
+            }
+        ]
     }
 ])
 
