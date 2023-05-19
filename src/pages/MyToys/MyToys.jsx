aa2f1822d003f18cc9cad5c78ce2747a0e01e148
mdiktushar <md.tushar1804@gmail.com>
2023-05-20 00:09:51 +0600

    myToys page is created

---
+++ src/pages/MyToys/MyToys.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyToys = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-5">
+      <h2 className="text-5xl">My toys</h2>
+    </div>
+  );
+};
+
+export default MyToys;
