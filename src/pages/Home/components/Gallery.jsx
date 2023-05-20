baaafec8191baf67d68ee5048c0dc1d26d972374
mdiktushar <md.tushar1804@gmail.com>
2023-05-20 21:48:43 +0600

    galary

--- src/pages/Home/components/Gallery.jsx
+++ src/pages/Home/components/Gallery.jsx
@@ -1,33 +1,58 @@
 import React from "react";
-import mikey from '../../../assets/img/mikey.jpg'
-import snow from '../../../assets/img/snow.jpg'
+import mikey from "../../../assets/img/mikey.jpg";
+import snow from "../../../assets/img/snow.jpg";
 
 const Gallery = () => {
   return (
-    <div className="mt-20">
-        <h2 className="text-5xl font-semibold text-gray-600 text-center">Gallery</h2>
-        <hr  className="mb-5"/>
+    <div className="mt-20 bg-slate-100 rounded-xl">
+      <h2 className="text-5xl font-semibold text-gray-600 text-center">
+        Gallery
+      </h2>
+      <hr className="mb-5" />
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 place-items-center">
         <img
-          src={`https://ae01.alicdn.com/kf/H66f7c9be009148958e23bed5ea3c3004x.jpg_640x640Q90.jpg_.webp`}
+          className="object-cover h-100 w-96 rounded-lg"
+          src="https://m.media-amazon.com/images/I/81fLB-HCipL._AC_UF894,1000_QL80_.jpg"
           alt=""
         />
         <img
-          src={`https://ae01.alicdn.com/kf/Hd79713902ca244bb91bf2e8f9715269eZ.jpg_640x640Q90.jpg_.webp`}
+          className="object-cover h-100 w-96 rounded-lg"
+          src="https://www.magicalearscollectibles.com/assets/images/scp1520922.jpg"
           alt=""
         />
         <img
-          src={`https://ae01.alicdn.com/kf/H9837ac3aa66e4c6ab6bf9417960aa25cS.jpg_640x640Q90.jpg_.webp`}
-          alt=""
-        />
-      </div>
-      <div className="grid sm:grid-cols-1 md:grid-cols-2 mt-4 gap-2 place-items-center">
-        <img
-          src={mikey}
+          className="object-cover h-100 w-96 rounded-lg"
+          src="https://cdn-ssl.s7.disneystore.com/is/image/DisneyShopping/6107000443926-1?fmt=jpeg&qlt=90&wid=608&hei=608"
           alt=""
         />
         <img
-          src={snow}
+          className="object-cover h-100 w-96 rounded-lg"
+          src="https://static.thcdn.com/images/large/original//productimg/1600/1600/13026529-1864861988029398.jpg"
+          alt=""
+        />
+        <img
+          className="object-cover h-100 w-96 rounded-lg"
+          src="https://image.smythstoys.com/original/desktop/222827_7.jpg"
+          alt=""
+        />
+        <img
+          className="object-cover h-100 w-96 rounded-lg"
+          src="https://m.media-amazon.com/images/I/81+C+-4x38L._AC_UF1000,1000_QL80_.jpg"
+          alt=""
+        />
+        <img
+          className="object-cover h-100 w-96 rounded-lg"
+          src={`https://m.media-amazon.com/images/I/71eGtb5sfnL._AC_SL1500_.jpg`}
+          alt=""
+        />
+        <img
+          className="object-cover h-100 w-96 rounded-lg"
+          src={`https://www.campusgifts.co.uk/media/catalog/product/cache/623a6c74e30aa11506d045198d97fecc/w/i/winnie-the-pooh-hundred-acre-woods-soft-toy-range-lr_1.jpg`}
+          alt=""
+        />
+        <img
+          className="object-cover h-100 w-96 rounded-lg"
+          src={`https://www.campusgifts.co.uk/media/catalog/product/cache/623a6c74e30aa11506d045198d97fecc/r/a/rainbow-designs-dn1471-tigger-soft-toy-1.jpg`}
           alt=""
         />
       </div>
