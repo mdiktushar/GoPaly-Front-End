abe3a328aa6c85816e83bab09b2bc4d0a3594fdd
mdiktushar <md.tushar1804@gmail.com>
2023-05-20 22:02:31 +0600

    aso gallery

--- src/pages/Home/components/Banner.jsx
+++ src/pages/Home/components/Banner.jsx
@@ -5,13 +5,13 @@ const Banner = () => {
     <div
       className="hero min-h-screen"
       style={{
-        backgroundImage: `url("https://mlpnk72yciwc.i.optimole.com/cqhiHLc.IIZS~2ef73/w:auto/h:auto/q:75/https://bleedingcool.com/wp-content/uploads/2022/12/320338579_1463900954133647_3352310553050570283_n-e1671304792530.jpg")`,
+        backgroundImage: `url("https://deadline.com/wp-content/uploads/2022/12/walt-disney-logo.jpg")`,
       }}
     >
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <h1 className="mb-5 text-5xl font-bold">Welcome</h1>
           <p className="mb-5">
             Welcome to our GoyPlay Disney Toy Wonderland! Immerse yourself in a world of
             enchantment and endless fun. Explore our captivating collection of
--- src/pages/Home/components/Gallery.jsx
+++ src/pages/Home/components/Gallery.jsx
@@ -1,8 +1,13 @@
-import React from "react";
-import mikey from "../../../assets/img/mikey.jpg";
-import snow from "../../../assets/img/snow.jpg";
+import React, { useEffect } from "react";
+// importing aos
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Gallery = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <div className="mt-20 bg-slate-100 rounded-xl">
       <h2 className="text-5xl font-semibold text-gray-600 text-center">
@@ -11,46 +16,64 @@ const Gallery = () => {
       <hr className="mb-5" />
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 place-items-center">
         <img
+          data-aos="fade-right"
+          data-aos-delay="500"
           className="object-cover h-100 w-96 rounded-lg"
           src="https://m.media-amazon.com/images/I/81fLB-HCipL._AC_UF894,1000_QL80_.jpg"
           alt=""
         />
         <img
+          data-aos="flip-left"
+          data-aos-delay="500"
           className="object-cover h-100 w-96 rounded-lg"
           src="https://www.magicalearscollectibles.com/assets/images/scp1520922.jpg"
           alt=""
         />
         <img
+          data-aos="fade-left"
+          data-aos-delay="500"
           className="object-cover h-100 w-96 rounded-lg"
           src="https://cdn-ssl.s7.disneystore.com/is/image/DisneyShopping/6107000443926-1?fmt=jpeg&qlt=90&wid=608&hei=608"
           alt=""
         />
         <img
+          data-aos="fade-right"
+          data-aos-delay="500"
           className="object-cover h-100 w-96 rounded-lg"
           src="https://static.thcdn.com/images/large/original//productimg/1600/1600/13026529-1864861988029398.jpg"
           alt=""
         />
         <img
+          data-aos="flip-left"
+          data-aos-delay="500"
           className="object-cover h-100 w-96 rounded-lg"
           src="https://image.smythstoys.com/original/desktop/222827_7.jpg"
           alt=""
         />
         <img
+          data-aos="fade-left"
+          data-aos-delay="500"
           className="object-cover h-100 w-96 rounded-lg"
           src="https://m.media-amazon.com/images/I/81+C+-4x38L._AC_UF1000,1000_QL80_.jpg"
           alt=""
         />
         <img
+          data-aos="fade-right"
+          data-aos-delay="500"
           className="object-cover h-100 w-96 rounded-lg"
           src={`https://m.media-amazon.com/images/I/71eGtb5sfnL._AC_SL1500_.jpg`}
           alt=""
         />
         <img
+          data-aos="flip-left"
+          data-aos-delay="500"
           className="object-cover h-100 w-96 rounded-lg"
           src={`https://www.campusgifts.co.uk/media/catalog/product/cache/623a6c74e30aa11506d045198d97fecc/w/i/winnie-the-pooh-hundred-acre-woods-soft-toy-range-lr_1.jpg`}
           alt=""
         />
         <img
+          data-aos="fade-left"
+          data-aos-delay="500"
           className="object-cover h-100 w-96 rounded-lg"
           src={`https://www.campusgifts.co.uk/media/catalog/product/cache/623a6c74e30aa11506d045198d97fecc/r/a/rainbow-designs-dn1471-tigger-soft-toy-1.jpg`}
           alt=""
